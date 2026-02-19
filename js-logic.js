4b42fed427f723eaffab2de30ddeac402541570b
Samiksha Awate <samikshaawate7@gmail.com>
2026-02-19 18:31:05 +0530

    Auto Play Button

--- js-logic.js
+++ js-logic.js
@@ -81,3 +81,21 @@ let score =JSON.parse(localStorage.getItem('score'));
         
         return computerMove;
     }
+
+    let isAutoPlaying = false;
+    let intervalId;
+
+    function autoPlay(){
+        if(!isAutoPlaying){
+            intervalId = setInterval(function(){
+            const playerMove = pickComputerMove();
+            PlayGame(playerMove);
+        },1000);
+        isAutoPlaying = true;
+        }
+        else{
+            clearInterval(intervalId);
+            isAutoPlaying = false;
+        }
+        
+    }
